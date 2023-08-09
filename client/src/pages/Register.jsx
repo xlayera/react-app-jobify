8e1d4a2bf6be0788d54a2659e09d9234f342109d
Ximena Layera <xlayera22@gmail.com>
2023-08-09 15:26:43 -0400

    feat - register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,37 @@
 import { Link } from "react-router-dom"
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage"
+import { Logo } from "../components"
 
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to='/login'> Login Page</Link>
-    </div>
+    <Wrapper>
+      <form action="" className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div>
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-input"
+            defaultValue=''
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-block"> Submit</button>
+        <p>Already a member?
+          <Link to='/Login' className="member-btn">
+            Login
+          </Link>
+        </p>
+
+      </form>
+
+    </Wrapper>
   )
 }
 
