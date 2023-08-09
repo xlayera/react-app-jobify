efa039548c0a1f2fd1c41e98851bf0d28047cc97
Ximena Layera <xlayera22@gmail.com>
2023-08-09 11:07:07 -0400

    style - landing page

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -2,10 +2,7 @@ import { Outlet } from "react-router-dom"
 
 
 const HomeLayout = () => {
-  return <div>
-    <nav>NavBar</nav>
-    <Outlet />
-  </div>
+  return <Outlet />
 }
 
 export default HomeLayout
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,8 @@
-import styled from "styled-components";
+//import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg"
+import logo from "../assets/images/logo.svg"
+import { Link } from "react-router-dom";
 
 /* const StyledBtn = styled.button`
   font-size: 1.5rem;
@@ -10,14 +14,39 @@ import styled from "styled-components";
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>Landing</h1>
+      <nav>
+        <img src={logo} alt="Jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, cum no dolore iudicabit,
+            id his enim prima. Cum an illum dicta reprimique,
+            eam enim aeque docendi id. Dicit saepe voluptatum vel ea.
+            No his amet conclusionemque, equidem sanctus vim eu.
+            Has ut exerci legimus civibus, mea et tantas facete.
+            Quo dicit feugait sensibus ne, sit ea nonumy facete tritani.
+          </p>
+          <Link to="/register" className='btn register-link'>
+            Register
+          </Link>
+          <Link to="/login" className='btn'>
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+      {/*   <h1>Landing</h1>
       <div className="content"> some content</div>
-      {/*  <StyledBtn>Styled btn</StyledBtn> */}
+      <StyledBtn>Styled btn</StyledBtn> */}
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
+/* const Wrapper = styled.div`
 background: red;
 h1{
   color:white;
@@ -26,6 +55,6 @@ h1{
   background:blue;
   color:yellow;
 }
-`;
+`; */
 
 export default Landing
